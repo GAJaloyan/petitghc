17627b1554534069ea9e878838e004e2f97bef6d
--replace-all <cyrille.ruggero@ens.fr>
2014-11-08 13:00:05 +0100

    Adds command line argument to the main file

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,8 @@
-let 
+let parseOnly = ref false in
+let main =
+   let speclist = [("--parse-only", Arg.Set parseOnly, "Enables parse only mode")]
+   in
+   let usageMessage = "Petitghc is a small compile for a subset of the Haskell language: call with ./petitghc filename or ./petitghc --parse-only filename" in
+   Arg.parse speclist parseFile usageMessage
+
+let () = main
