0697e27539f6dd5f98da9aec338ed939a036983a
GAJaloyan <georges-axel.jaloyan@ens.fr>
2016-03-09 13:22:12 -0800

    travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,6 +3,10 @@ git config --global user.email "travis@example.com"
 git config --global user.name "Travis CI"
 
 install_on_linux () {
+    sudo add-apt-repository main
+    sudo add-apt-repository universe
+    sudo add-apt-repository restricted
+    sudo add-apt-repository multiverse
     sudo apt-get update -qq
     sudo apt-get install -qq opam
     sudo apt-get install -qq spim
