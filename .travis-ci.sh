32080cbdf9d8dd08d377b2f95838d2390e40e202
GAJaloyan <georges-axel.jaloyan@ens.fr>
2016-03-09 11:44:19 -0800

    travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,16 +3,7 @@ git config --global user.email "travis@example.com"
 git config --global user.name "Travis CI"
 
 install_on_linux () {
-    # Install OCaml PPAs
-    case "$OCAML_VERSION" in
-        3.12.1) ppa=avsm/ocaml312+opam12 ;;
-        4.00.1) ppa=avsm/ocaml40+opam12 ;;
-        4.01.0) ppa=avsm/ocaml41+opam12 ;;
-        4.02.3) ppa=avsm/ocaml42+opam12 ;;
-        *) echo Unknown $OCAML_VERSION; exit 1 ;;
-    esac
-
-    echo "yes" | sudo add-apt-repository ppa:$ppa
+    sudo add-apt-repository ppa:$ppa
     sudo apt-get update -qq
     sudo apt-get install -qq opam
     sudo apt-get install -qq spim
